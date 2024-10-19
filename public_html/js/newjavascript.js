6bf752faef20c32c6483df7223a0769205177572
Egorchik228 <egordorofejcuk@gmail.com>
2024-10-19 20:45:31 +0300

    добавил класс и функцию показа крестика или нолика

--- public_html/js/newjavascript.js
+++ public_html/js/newjavascript.js
@@ -1,7 +1,26 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+class Figure {
+    constructor(x, y, type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+}
+let moves = [];
+let key = 0;
+document.querySelectorAll('.toggleBtn').forEach(button => {
+    button.addEventListener('click', function () {
+        let obj = new Figure(this.getAttribute('data-x'), this.getAttribute('data-y'), key === 0 ? "X" : "O");
+        this.style.color = "black";
+        this.disabled = true;
+        if (key === 0) {
+            this.textContent = 'X';
+            key = 1;
+            document.getElementById("text").textContent = "Ход нолика";
+        } else {
+            this.textContent = 'O';
+            key = 0;
+            document.getElementById("text").textContent = "Ход крестика";
+        }
+        moves.push(obj);
+    });
+});
