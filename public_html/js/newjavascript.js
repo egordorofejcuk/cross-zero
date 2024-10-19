6338df58b0c4c64b3aa41b52f57bd3f9f3e1180a
Egorchik228 <egordorofejcuk@gmail.com>
2024-10-19 20:57:10 +0300

    добавил функцию проверки результата игры

--- public_html/js/newjavascript.js
+++ public_html/js/newjavascript.js
@@ -30,3 +30,44 @@ function disableBut() {
         button.disabled = true;
     });
 }
+
+function check() {
+    let horizontalx = 0;
+    let verticalx = 0;
+    let diagonal1x = 0;
+    let diagonal1y = 0;
+    let horizontaly = 0;
+    let verticaly = 0;
+    let diagonal2x = 0;
+    let diagonal2y = 0;
+    let lastMove = moves[moves.length - 1];
+    for (let i = 0; i < moves.length; i++) {
+        if (moves[i].x === lastMove.x && moves[i].type === lastMove.type) {
+            if (moves[i].type === "X") horizontalx++;
+            else horizontaly++;
+        }
+        if (moves[i].y === lastMove.y && moves[i].type === lastMove.type) {
+            if (moves[i].type === "X") verticalx++;
+            else verticaly++;
+        }
+        if (moves[i].x === moves[i].y) {
+            if (moves[i].type === "X") diagonal1x++;
+            else diagonal1y++;
+        }
+        if (parseInt(moves[i].x) + parseInt(moves[i].y) === 4) {
+            if (moves[i].type === "X") diagonal2x++;
+            else diagonal2y++;
+        }
+    }
+    if (horizontalx === 3 || verticalx === 3 || diagonal1x === 3 || diagonal2x === 3) {
+        document.getElementById("text").textContent = "Крестики победили";
+        disableBut();
+    }
+    else if (horizontaly === 3 || verticaly === 3 || diagonal1y === 3 || diagonal2y === 3) {
+        document.getElementById("text").textContent = "Нолики победили";
+        disableBut();
+    }
+    else if (moves.length === 9) {
+        document.getElementById("text").textContent = "Ничья";
+    }
+}
